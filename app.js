d6ffda42e05bfbf84aae1f085079304291eb8eea
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-08-27 17:16:59 +0700

    fix

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
